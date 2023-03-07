1e84b76170ce72dd6b821e0c51492bdb62610f64
P-CH <P-CH.44n@gmx.de>
2023-03-07 14:23:47 +0100

    Replaced commas at line endings

--- helpers/errors.js
+++ helpers/errors.js
@@ -2,7 +2,7 @@ class DirTravErr extends Error {
     constructor(message = "Client possibly attempted directory traversal!"){
         super(message);
         this.tcolor = "red";
-        this.type = "Alert",
+        this.type = "Alert";
         this.scolor = "yellow";
         this.status = 308;
     }
@@ -11,7 +11,7 @@ class BelowRootErr extends Error {
     constructor(message = "Client tried to access a file/directory below the root!"){
         super(message);
         this.tcolor = "yellow";
-        this.type = "Warn",
+        this.type = "Warn";
         this.scolor = "yellow";
         this.status = 307;
     }
@@ -20,7 +20,7 @@ class SubfolderErr extends Error {
     constructor(message = "Client tried to access subfolder!"){
         super(message);
         this.tcolor = "green";
-        this.type = "Info",
+        this.type = "Info";
         this.scolor = "red";
         this.status = 403;
     }
@@ -29,7 +29,7 @@ class InterprocErr extends Error {
     constructor(message = "Requested resource is used for inter-process communication!"){
         super(message);
         this.tcolor = "magenta";
-        this.type = "Error",
+        this.type = "Error";
         this.scolor = "red";
         this.status = 415;
     }
